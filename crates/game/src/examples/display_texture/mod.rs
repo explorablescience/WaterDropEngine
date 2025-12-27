f5d05faf77bd19b84bf32746ed1c37bd4d169816
Maxime D. <xam4lor@gmail.com>
2025-12-27 22:50:59 +0100

    SImplified the example mesh declaration

--- crates/game/src/examples/display_texture/mod.rs
+++ crates/game/src/examples/display_texture/mod.rs
@@ -4,24 +4,15 @@ mod pipeline;
 mod pass;
 mod material;
 
-use wde_render::{assets::{MaterialsPluginRegister, RenderAssetsPlugin, Texture, TextureLoaderSettings}, core::RenderApp, passes::render_graph::RenderGraph};
-use wde_wgpu::texture::WTextureFormat;
+use wde_render::{assets::{MaterialsPluginRegister, Mesh, MeshAsset, ModelBoundingBox, RenderAssetsPlugin, Texture, TextureLoaderSettings}, core::RenderApp, passes::render_graph::RenderGraph};
+use wde_wgpu::{texture::WTextureFormat, vertex::WVertex};
 
-use crate::examples::display_texture::{material::{DisplayTextureMaterial, DisplayTextureMaterialAsset}, pass::{DisplayTexturePass, GPUMaterialHandler, RenderPassMesh}, pipeline::{DisplayTexturePipeline, DisplayTexturePipelineAsset, GpuDisplayTexturePipeline}};
+use crate::examples::display_texture::{material::{DisplayTextureMaterial, DisplayTextureMaterialAsset}, pass::{DisplayTexturePass, RenderPassEntity}, pipeline::{DisplayTexturePipeline, DisplayTexturePipelineAsset, GpuDisplayTexturePipeline}};
 
 pub struct DisplayTextureComponentPlugin;
 impl Plugin for DisplayTextureComponentPlugin {
     fn build(&self, app: &mut App) {
-        // Create the render pass mesh resource
-        app
-            .init_resource::<RenderPassMesh>()
-            .add_systems(Startup, RenderPassMesh::init);
-        app.get_sub_app_mut(RenderApp).unwrap()
-            .init_resource::<RenderPassMesh>();
-
         // Register the material
-        app.get_sub_app_mut(RenderApp).unwrap()
-            .init_resource::<GPUMaterialHandler>();
         app.add_plugins(MaterialsPluginRegister::<DisplayTextureMaterialAsset>::default());
         app.register_type::<DisplayTextureMaterial>();
 
@@ -32,6 +23,7 @@ impl Plugin for DisplayTextureComponentPlugin {
 
         // Add the render passes
         let mut render_graph = app.get_sub_app_mut(RenderApp).unwrap()
+            .init_resource::<RenderPassEntity>()
             .world_mut().get_resource_mut::<RenderGraph>().unwrap();
         render_graph.add_pass::<DisplayTexturePass>(2);
 
@@ -59,6 +51,25 @@ pub fn load_entity(mut commands: Commands, server: Res<AssetServer>) {
         texture: Some(texture)
     });
 
+    // Create the mesh
+    let mesh: Handle<MeshAsset> = server.add(MeshAsset {
+        label: "texture-display-pass".to_string(),
+        vertices: vec![
+            WVertex { position: [-1.0, 1.0, 0.0], uv: [0.0, 1.0], normal: [0.0, 0.0, 0.0] },
+            WVertex { position: [-1.0, -1.0, 0.0], uv: [0.0, 0.0], normal: [0.0, 0.0, 0.0] },
+            WVertex { position: [1.0, -1.0, 0.0], uv: [1.0, 0.0], normal: [0.0, 0.0, 0.0] },
+            WVertex { position: [1.0, 1.0, 0.0], uv: [1.0, 1.0], normal: [0.0, 0.0, 0.0] },
+        ],
+        indices: vec![0, 1, 2, 0, 2, 3],
+        bounding_box: ModelBoundingBox {
+            min: Vec3::new(-1.0, -1.0, 0.0),
+            max: Vec3::new(1.0, 1.0, 0.0),
+        },
+    });
+
     // Create the entity
-    commands.spawn(DisplayTextureMaterial(material));
+    commands.spawn((
+        DisplayTextureMaterial(material),
+        Mesh(mesh)
+    ));
 }
--- crates/game/src/examples/display_texture/pass.rs
+++ crates/game/src/examples/display_texture/pass.rs
@@ -1,59 +1,27 @@
 use bevy::prelude::*;
-use wde_render::{assets::{GpuMaterial, GpuMesh, MeshAsset, ModelBoundingBox, RenderAssets}, core::SwapchainFrame, passes::render_graph::RenderPass, pipelines::{CachedPipelineStatus, PipelineManager}};
-use wde_wgpu::{command_buffer::{RenderPassBuilder, RenderPassColorAttachment, WCommandBuffer}, instance::WRenderInstance, vertex::WVertex};
+use wde_render::{assets::{GpuMaterial, GpuMesh, Mesh, MeshAsset, RenderAssets}, core::SwapchainFrame, passes::render_graph::RenderPass, pipelines::{CachedPipelineStatus, PipelineManager}};
+use wde_wgpu::{command_buffer::{RenderPassBuilder, RenderPassColorAttachment, WCommandBuffer}, instance::WRenderInstance};
 
 use crate::examples::display_texture::{material::{DisplayTextureMaterial, DisplayTextureMaterialAsset}, pipeline::GpuDisplayTexturePipeline};
 
 #[derive(Resource, Default)]
-pub struct RenderPassMesh {
-    mesh: Option<Handle<MeshAsset>>
-}
-impl RenderPassMesh {
-    // Creates the rendering mesh.
-    pub fn init(assets_server: Res<AssetServer>, mut render_pass: ResMut<RenderPassMesh>) {
-        // Create the 2d quad mesh
-        let deferred_mesh: Handle<MeshAsset> = assets_server.add(MeshAsset {
-            label: "texture-display-pass".to_string(),
-            vertices: vec![
-                WVertex { position: [-1.0, 1.0, 0.0], uv: [0.0, 1.0], normal: [0.0, 0.0, 0.0] },
-                WVertex { position: [-1.0, -1.0, 0.0], uv: [0.0, 0.0], normal: [0.0, 0.0, 0.0] },
-                WVertex { position: [1.0, -1.0, 0.0], uv: [1.0, 0.0], normal: [0.0, 0.0, 0.0] },
-                WVertex { position: [1.0, 1.0, 0.0], uv: [1.0, 1.0], normal: [0.0, 0.0, 0.0] },
-            ],
-            indices: vec![0, 1, 2, 0, 2, 3],
-            bounding_box: ModelBoundingBox {
-                min: Vec3::new(-1.0, -1.0, 0.0),
-                max: Vec3::new(1.0, 1.0, 0.0),
-            },
-        });
-        render_pass.mesh = Some(deferred_mesh);
-    }
-}
-
-#[derive(Resource, Default)]
-pub struct GPUMaterialHandler {
-    pub material: Option<Handle<DisplayTextureMaterialAsset>>,
+pub struct RenderPassEntity {
+    pub mesh: Handle<MeshAsset>,
+    pub material: Handle<DisplayTextureMaterialAsset>,
 }
 
 #[derive(Resource, Default)]
 pub struct DisplayTexturePass;
 impl RenderPass for DisplayTexturePass {
     fn extract(&self, main_world: &mut World, render_world: &mut World) {
-        // Extract the material handle
-        let material_handle = match main_world.query_filtered::<&DisplayTextureMaterial, With<DisplayTextureMaterial>>().iter(main_world).next() {
-            Some(material) => material.0.clone(),
+        // Extract the mesh and material handles
+        let entity = match main_world.query::<(Entity, &DisplayTextureMaterial, &Mesh)>().iter(main_world).next() {
+            Some((entity, material, mesh)) => (entity, material.0.clone(), mesh.0.clone()),
             None => return
         };
-        let mut material_handler = render_world.get_resource_mut::<GPUMaterialHandler>().unwrap();
-        material_handler.material = Some(material_handle);
-
-        // Extract the mesh for rendering
-        let mesh_cpu = main_world.get_resource::<RenderPassMesh>().unwrap();
-        let mut render_pass = render_world.get_resource_mut::<RenderPassMesh>().unwrap();
-        render_pass.mesh = None;
-        if let Some(ref mesh_cpu) = mesh_cpu.mesh {
-            render_pass.mesh = Some(mesh_cpu.clone());
-        }
+        let mut render_pass_entity = render_world.get_resource_mut::<RenderPassEntity>().unwrap();
+        render_pass_entity.material = entity.1;
+        render_pass_entity.mesh = entity.2;
     }
 
     fn render(&self, world: &mut World) {
@@ -62,28 +30,15 @@ impl RenderPass for DisplayTexturePass {
         let render_instance = render_instance.data.read().unwrap();
         let swapchain_frame = world.get_resource::<SwapchainFrame>().unwrap().data.as_ref().unwrap();
 
-        // Check if material is ready
-        let material_handle = match &world.get_resource::<GPUMaterialHandler>().unwrap().material {
-            Some(material) => material,
-            None => return
-        };
-
-        // Check if mesh is ready
-        let meshes = world.get_resource::<RenderAssets<GpuMesh>>().unwrap();
-        let mesh = match &world.get_resource::<RenderPassMesh>().unwrap().mesh {
-            Some(mesh) => match meshes.get(mesh) {
-                Some(mesh) => mesh,
-                None => return
-            },
-            None => return
-        };
-
         // Check if pipeline is ready
         let pipeline_manager = world.get_resource::<PipelineManager>().unwrap();
         let pipeline = match world.get_resource::<RenderAssets<GpuDisplayTexturePipeline>>().unwrap().iter().next() {
             Some((_, pipeline)) => pipeline,
             None => return
         };
+
+        // Get the entity to render
+        let entity = world.get_resource::<RenderPassEntity>().unwrap();
         
         // Render the texture
         let mut command_buffer = WCommandBuffer::new(&render_instance, "display-texture");
@@ -95,13 +50,16 @@ impl RenderPass for DisplayTexturePass {
                 });
             });
 
+            let meshes = world.get_resource::<RenderAssets<GpuMesh>>().unwrap();
             let materials = world.get_resource::<RenderAssets<GpuMaterial<DisplayTextureMaterialAsset>>>().unwrap();
             if let (
                 CachedPipelineStatus::OkRender(pipeline),
-                Some(material)
+                Some(material),
+                Some(mesh)
             ) = (
                 pipeline_manager.get_pipeline(pipeline.cached_pipeline_index),
-                materials.get(material_handle)
+                materials.get(&entity.material),
+                meshes.get(&entity.mesh)
             ) {
                 // Set the pipeline
                 if render_pass.set_pipeline(pipeline).is_ok() {
