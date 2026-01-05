35ad55eee62054ee06af2fa64252769952b25a3d
Maxime D. <xam4lor@gmail.com>
2026-01-05 20:04:40 +0100

    Updated scene

--- crates/scene/wde-scene/src/lib.rs
+++ crates/scene/wde-scene/src/lib.rs
@@ -1,5 +1,5 @@
 use wde_logger::prelude::*;
-use bevy::{prelude::*, window::PrimaryWindow};
+use bevy::prelude::*;
 use wde_camera::prelude::*;
 use wde_pbr::prelude::*;
 use wde_physics::prelude::*;
@@ -10,17 +10,13 @@ pub struct ScenePlugin;
 impl Plugin for ScenePlugin {
     fn build(&self, app: &mut App) {
         app
-            .insert_resource(TimerResource(Timer::from_seconds(5.0, TimerMode::Once)))
-            .add_systems(Startup, init_scene)
-            .add_systems(Update, remove_after_time)
-            .add_systems(Update, cast_ray);
+            // .insert_resource(TimerResource(Timer::from_seconds(5.0, TimerMode::Once)))
+            .add_systems(Startup, init_scene);
     }
 }
 
-#[derive(Resource)]
-struct CubeEntity(Entity);
-#[derive(Resource)]
-struct GroundEntity(Entity);
+// #[derive(Resource)]
+// struct GroundEntity(Entity);
 
 fn init_scene(mut commands: Commands, asset_server: Res<AssetServer>, mut pbrmaterials: ResMut<Assets<PbrMaterialAsset>>, mut gizmomaterials: ResMut<Assets<GizmoMaterialAsset>>, mut meshes: ResMut<Assets<MeshAsset>>) {
     // Main camera
@@ -32,30 +28,43 @@ fn init_scene(mut commands: Commands, asset_server: Res<AssetServer>, mut pbrmat
         ActiveCamera
     ));
 
-    // Spawn a cube centered at the origin
-    let cube_entity = commands.spawn((
-        Transform::from_xyz(0.0, 0.0, 0.0).with_scale(Vec3::ONE * 0.1),
-        Mesh(asset_server.load("models/container.obj")),
+    // Spawn a house centered at the origin
+    commands.spawn((
+        Transform::from_xyz(0.0, 0.0, 0.0).with_scale(Vec3::ONE * 0.05),
+        Mesh(asset_server.load("houses/house_demo1.obj")),
         PbrMaterial(pbrmaterials.add(PbrMaterialAsset {
-            label: "red".to_string(),
-            albedo: (1.0, 0.0, 0.0, 1.0),
+            label: "house".to_string(),
+            albedo: (0.8, 0.8, 0.8, 1.0),
             specular: 0.5,
             ..Default::default()
         }))
-    )).id();
-    commands.insert_resource(CubeEntity(cube_entity));
+    ));
+
+    // // Spawn a cube centered at the origin
+    // let cube_entity = commands.spawn((
+    //     Transform::from_xyz(0.0, 0.0, 0.0).with_scale(Vec3::ONE * 0.1),
+    //     Mesh(asset_server.load("models/container.obj")),
+    //     PbrMaterial(pbrmaterials.add(PbrMaterialAsset {
+    //         label: "red".to_string(),
+    //         albedo: (1.0, 0.0, 0.0, 1.0),
+    //         specular: 0.5,
+    //         ..Default::default()
+    //     }))
+    // )).id();
+    // commands.insert_resource(CubeEntity(cube_entity));
 
     // Create the ground
-    let ground = commands.spawn((
-        Transform::from_xyz(0.0, -1.0, 0.0).with_scale(Vec3::ONE * 100.0),
-        Mesh(meshes.add(PlaneMesh::from("ground", 100, Vec3::Y))),
+    let scaling: u32 = 25; // Must be odd
+    let _ground = commands.spawn((
+        Transform::from_xyz(0.0, -0.0001, 0.0).with_scale(Vec3::ONE * scaling as f32),
+        Mesh(meshes.add(PlaneMesh::from("ground", scaling, Vec3::Y))),
         GizmoMaterial(gizmomaterials.add(GizmoMaterialAsset {
             label: "grid".to_string(),
             color: [0.8, 0.8, 0.8, 1.0],
         })),
         Collider::cuboid(50.0, 0.1, 50.0),
     )).id();
-    commands.insert_resource(GroundEntity(ground));
+    // commands.insert_resource(GroundEntity(ground));
 
     // Spawn the lights
     commands.spawn(DirectionalLight {
@@ -64,41 +73,41 @@ fn init_scene(mut commands: Commands, asset_server: Res<AssetServer>, mut pbrmat
     });
 }
 
-fn cast_ray(
-    mut commands: Commands,
-    phworld: Res<PhysicsWorld>,
-    window: Single<&Window, With<PrimaryWindow>>,
-    cube_entity_res: Res<CubeEntity>,
-    camera_query: Query<(&Transform, &CameraView), With<Camera>>)
-{
-    // Create the ray from ndc position
-    let cursor_ndc_position = match window.cursor_position() {
-        Some(pos) => pos / window.size(),
-        None => return,
-    };
-    let (camera_transform, camera_view) = camera_query.single().map_err(|_| "No camera found").unwrap();
-    let ray = Ray::from_ndc(cursor_ndc_position, window.size().x / window.size().y, camera_transform, camera_view);
+// fn cast_ray(
+//     mut commands: Commands,
+//     phworld: Res<PhysicsWorld>,
+//     window: Single<&Window, With<PrimaryWindow>>,
+//     cube_entity_res: Res<CubeEntity>,
+//     camera_query: Query<(&Transform, &CameraView), With<Camera>>)
+// {
+//     // Create the ray from ndc position
+//     let cursor_ndc_position = match window.cursor_position() {
+//         Some(pos) => pos / window.size(),
+//         None => return,
+//     };
+//     let (camera_transform, camera_view) = camera_query.single().map_err(|_| "No camera found").unwrap();
+//     let ray = Ray::from_ndc(cursor_ndc_position, window.size().x / window.size().y, camera_transform, camera_view);
 
-    // Cast the ray in the physics world
-    if let Some((_, toi)) = phworld.as_ref().cast_ray(&ray, &RayCastConfig::default()) {
-        let hit_point = ray.point_at(toi);
+//     // Cast the ray in the physics world
+//     if let Some((_, toi)) = phworld.as_ref().cast_ray(&ray, &RayCastConfig::default()) {
+//         let hit_point = ray.point_at(toi);
 
-        // Move the cube up to the position of the hit point.
-        commands.entity(cube_entity_res.0).insert(Transform::from_translation(hit_point));
-    }
-}
+//         // Move the cube up to the position of the hit point.
+//         commands.entity(cube_entity_res.0).insert(Transform::from_translation(hit_point));
+//     }
+// }
 
-#[derive(Resource)]
-struct TimerResource(Timer);
+// #[derive(Resource)]
+// struct TimerResource(Timer);
 
-fn remove_after_time(
-    mut commands: Commands,
-    time: Res<Time>,
-    ground_entity: Res<GroundEntity>,
-    mut timer: ResMut<TimerResource>,
-) {
-    if timer.0.tick(time.delta()).just_finished() {
-        info!("Removing cube after 5 seconds");
-        commands.entity(ground_entity.0).despawn();
-    }
-}
+// fn remove_after_time(
+//     mut commands: Commands,
+//     time: Res<Time>,
+//     ground_entity: Res<GroundEntity>,
+//     mut timer: ResMut<TimerResource>,
+// ) {
+//     if timer.0.tick(time.delta()).just_finished() {
+//         info!("Removing cube after 5 seconds");
+//         commands.entity(ground_entity.0).despawn();
+//     }
+// }
